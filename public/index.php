909ba76a06349da99064869f43b0f8cd22ff82ca
Dima Gudkov <d.o.gudkov@gmail.com>
2021-09-01 20:12:50 +0300

    add local fix for cors

--- public/index.php
+++ public/index.php
@@ -4,6 +4,14 @@ use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
+// Dirty hack disable CORS for local testing React app
+// Don't use it on production mode, use nginx or Docker
+//  
+// header('Access-Control-Allow-Origin: *');
+// header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+// header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+// header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
